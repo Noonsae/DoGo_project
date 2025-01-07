5c48f8793aeb390cdb559540012f05652e1ea451
Noon_sae <minseock7308@gmail.com>
2025-01-08 07:53:44 +0900

    📝 my-page로 가는 link 누락 확인 후 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import MyPageIcon from '../common/MyPageIcon';
-import HamburgerBtn from '../common/HamburgerBtn';
+import MyPageIcon from './navigator/MyPageIcon';
+import HamburgerBtn from './navigator/HamburgerBtn';
 import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className="w-full h-[86px] fixed">
       <div className="w-full max-w-[1200px] h-[86px] mx-auto flex flex-row justify-between items-center border border-black">
-        <h1 className='text-[32px] font-black'>          
+        <h1 className="text-[32px] font-black">
           <Link href="/">DoGo</Link>
         </h1>
         <div className="flex flex-row">
-          <MyPageIcon />
+          <Link href="/my-page">
+            <MyPageIcon />
+          </Link>
           <HamburgerBtn />
         </div>
       </div>
