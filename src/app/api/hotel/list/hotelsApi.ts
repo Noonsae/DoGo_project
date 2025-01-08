6b2642ef8d0f15f6fbecc6e19d5a8d3744a96920
wooseok <youkn0wthat@naver.com>
2025-01-08 20:12:37 +0900

    ✏️ hotelsApi 주석처리 다른 api를 참조해서 연결 했으나 수정사항이 있다면 다시 사용할 목적으로 남겨둠

--- src/app/api/hotel/list/hotelsApi.ts
+++ src/app/api/hotel/list/hotelsApi.ts
@@ -1,34 +1,32 @@
-import { browserSupabase } from '@/supabase/supabase-client';
-import { Tables } from '@/types/supabase/supabase-type';
+// import { serverSupabase } from '@/supabase/supabase-server';
+// import { HotelType } from '@/types/supabase/hotel-type';
+// import { RoomType } from '@/types/supabase/room-type';
 
-export type HotelsType = Tables<'hotels'>;
-export type RoomsType = Tables<'rooms'>;
+// interface Filters {
+//   grade?: number; // 호텔 등급
+// }
 
-interface Filters {
-  grade?: number; // 호텔 등급
-}
+// export const fetchHotelsAndRooms = async (
+//   filters: Filters = {}
+// ): Promise<{ hotels: HotelType[]; rooms: RoomType[] }> => {
+//   const supabase = await serverSupabase();
 
-export const fetchHotelsAndRooms = async (
-  filters: Filters = {}
-): Promise<{ hotels: HotelsType[]; rooms: RoomsType[] }> => {
-  const supabase = browserSupabase();
+//   let query = supabase.from('hotels').select('*');
 
-  let query = supabase.from('hotels').select('*');
+//   // 호텔 등급 필터 적용
+//   if (filters.grade) {
+//     query = query.eq('stars', filters.grade);
+//   }
 
-  // 호텔 등급 필터 적용
-  if (filters.grade) {
-    query = query.eq('stars', filters.grade);
-  }
+//   const { data: hotels, error: hotelsError } = await query;
+//   if (hotelsError) {
+//     throw new Error(`Failed to fetch hotels: ${hotelsError.message}`);
+//   }
 
-  const { data: hotels, error: hotelsError } = await query;
-  if (hotelsError) {
-    throw new Error(`Failed to fetch hotels: ${hotelsError.message}`);
-  }
+//   const { data: rooms, error: roomsError } = await supabase.from('rooms').select('*');
+//   if (roomsError) {
+//     throw new Error(`Failed to fetch rooms: ${roomsError.message}`);
+//   }
 
-  const { data: rooms, error: roomsError } = await supabase.from('rooms').select('*');
-  if (roomsError) {
-    throw new Error(`Failed to fetch rooms: ${roomsError.message}`);
-  }
-
-  return { hotels: hotels || [], rooms: rooms || [] };
-};
+//   return { hotels: hotels || [], rooms: rooms || [] };
+// };
