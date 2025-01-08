e560cfed2f9039a8bcaf104e2aef8e2cbe696a8e
hansolChoi29 <ggksthf29@gmail.com>
2025-01-08 14:26:15 +0900

    ✏️ 소셜로그인 중 pull받으려고 일단 commit중

--- src/app/api/auth/kakao/route.ts
+++ src/app/api/auth/kakao/route.ts
@@ -1,35 +1,33 @@
-// 'use server';
+'use server';
 
-// import { NextResponse } from 'next/server';
-// import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+import { createClient } from '@supabase/supabase-js'; 
 
-// export async function GET(request: Request) {
-//   const { searchParams, origin } = new URL(request.url);
-//   const code = searchParams.get('code'); // 카카오 인증 코드
-//   const next = searchParams.get('next') ?? '/'; // 리디렉션 경로 설정
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get('code'); // 카카오 인증 코드
+  const next = searchParams.get('next') ?? '/'; // 리디렉션 경로 설정
 
-//   if (code) {
-//     const supabase = await createClient(); // 서버 클라이언트 생성
-//     const { error } = await supabase.auth.exchangeCodeForSession(code); // 인증 코드로 세션 교환
+  if (code) {
+    const supabase = await createClient(); // 서버 클라이언트 생성
+    const { error } = await supabase.auth.exchangeCodeForSession(code); // 인증 코드로 세션 교환
 
-//     if (!error) {
-//       const forwardedHost = request.headers.get('x-forwarded-host'); // 로드 밸런서 호스트
-//       const isLocalEnv = process.env.NODE_ENV === 'development';
+    if (!error) {
+      const forwardedHost = request.headers.get('x-forwarded-host'); // 로드 밸런서 호스트
+      const isLocalEnv = process.env.NODE_ENV === 'development';
 
-//       if (isLocalEnv) {
-//         // 로컬 환경일 경우 리디렉션 처리
-//         return NextResponse.redirect(`${origin}${next}`);
-//       } else if (forwardedHost) {
-//         // 실제 배포 환경일 경우 처리
-//         return NextResponse.redirect(`https://${forwardedHost}${next}`);
-//       } else {
-//         return NextResponse.redirect(`${origin}${next}`);
-//       }
-//     }
-//   }
+      if (isLocalEnv) {
+        // 로컬 환경일 경우 리디렉션 처리
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        // 실제 배포 환경일 경우 처리
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
+  }
 
-//   // 인증 실패 시 에러 페이지로 리디렉션
-//   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
-// }
-
-//위코드는 공식문서에서 가져온 코드인데, 수퍼베이스동작을 먼저하고나서 처리되기때문에 토큰이 그대로 남는 결과초래.
+  // 인증 실패 시 에러 페이지로 리디렉션
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
