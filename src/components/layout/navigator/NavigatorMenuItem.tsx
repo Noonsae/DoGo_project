e5a719b44902139257d0627e8d26b06dd6befcc9
Noon_sae <minseock7308@gmail.com>
2025-01-08 09:57:27 +0900

    ♻️ navigatorMenuItem 컴포넌트 분리

--- src/components/layout/navigator/NavigatorMenuItem.tsx
+++ src/components/layout/navigator/NavigatorMenuItem.tsx
@@ -5,7 +5,7 @@ const NavigationMenuItem = ({ href, label }: { href: string; label: string }) =>
     <li>
       <Link
         href={href}
-        className="px-[16px] py-[12px] w-full h-[43px] flex items-center text-[16px] text-[#2c2c2c] font-regular hover:text-blue-500 rounded-[8px] hover:bg-[#f8f8f8]"
+        className="px-[16px] py-[12px] w-full h-[43px] flex items-center text-base text-[#2c2c2c] font-normal hover:text-blue-500 rounded-[8px] hover:bg-[#f8f8f8]"
       >
         {label}
       </Link>
