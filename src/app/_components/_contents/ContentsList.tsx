4687ebce334f3f90a422c8ecfb2e71fb20bc815f
Noon_sae <minseock7308@gmail.com>
2025-01-16 04:30:47 +0900

    🚚 컨텐츠에 맞게 파일 이름 변경

--- src/app/_components/_contents/ContentsList.tsx
+++ src/app/_components/_contents/ContentsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import TopPicks from './TopPicks';
+import Recommend from './Recommend';
 import History from './History';
 import HotelByView from './HotelByView';
 import HotelByRegion from './HotelByRegion';
@@ -7,8 +7,8 @@ import HotelByRegion from './HotelByRegion';
 const ContentsList = () => {
   return (
     <>
-      <TopPicks />
       <History />
+      <Recommend />
       <HotelByRegion />
       <HotelByView />
     </>
