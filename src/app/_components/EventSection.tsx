cd6908a3d976f85e24666ee6aacd986bea2c2f28
Noon_sae <minseock7308@gmail.com>
2025-01-16 04:29:17 +0900

    💄 event section 디자인 시안 반영

--- src/app/_components/EventSection.tsx
+++ src/app/_components/EventSection.tsx
@@ -1,20 +1,25 @@
+import Link from 'next/link';
 import React from 'react'
 
 const EventSection = () => {
   return (
-    <section className="w-full max-w-[1200px] h-[360px] mx-auto pt-[80px] pb-[60px] grid grid-cols-2">
+    <section className="w-full max-w-[1300px] px-[50px] h-[440px] mx-auto pt-[80px] pb-[60px] grid grid-cols-2 gap-[35px] text-[20px] text-[#636363] font-medium">
       {/* 스와이퍼로 슬라이드 들어가 들어갈 예정 */}
-      <div className="w-[585px] h-[300px] bg-[#E3E3E3] px-[40px] rounded-[8px] flex flex-col justify-evenly">
-        <p>Event 01</p>
-        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure fugit rem repudiandae nam eum ipsum sint temporibus eveniet officiis voluptatum!</span>
-        <a href="#" className='w-full text-right'>이벤트 보러가기</a>
-      </div> 
-      <div className="w-[585px] h-[300px] bg-[#E3E3E3] px-[40px] rounded-[8px] flex flex-col justify-evenly">
-        <p>Event 02</p>
-        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure fugit rem repudiandae nam eum ipsum sint temporibus eveniet officiis voluptatum!</span>
-        <a href="#" className='w-full text-right'>이벤트 보러가기</a>
-      </div> 
-  
+      <article className="relative w-full max-w-[580px] h-[300px] p-[40px] bg-[#EFEFEF] rounded-[8px]">
+        <h4 className="text-[32px] font-semibold">Event 01</h4>
+        <p className="mt-[12px]">Lorem, ipsum.</p>
+        <Link href="/event" className="absolute right-[40px] bottom-[40px] w-full p-[10px] text-right">
+          이벤트 보러가기
+        </Link>
+      </article>
+
+      <article className="relative w-full max-w-[580px] h-[300px] p-[40px] bg-[#EFEFEF] rounded-[8px]">
+        <h4 className="text-[32px] font-semibold">Event 02</h4>
+        <p className="mt-[12px]">Lorem, ipsum dolor.</p>
+        <Link href="/event" className="absolute right-[40px] bottom-[40px] w-full p-[10px] text-right">
+          이벤트 보러가기
+        </Link>
+      </article>
     </section>
   );
 }
