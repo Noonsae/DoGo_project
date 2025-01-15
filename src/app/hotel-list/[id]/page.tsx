450aad7dad6800573bdc7e0d5e64786f87d3c463
wooseok <youkn0wthat@naver.com>
2025-01-15 19:39:00 +0900

    ♻️ Navigation 컴포넌트 분리

--- src/app/hotel-list/[id]/page.tsx
+++ src/app/hotel-list/[id]/page.tsx
@@ -8,12 +8,11 @@ import { RoomType } from '@/types/supabase/room-type';
 import HotelRoom from './_components/HotelRoom';
 import HotelLocation from './_components/HotelLocation';
 import HotelBox from './_components/HotelBox';
-import useFormatCurrency from '@/hooks/formatCurrency/useFormatCurrency';
 import { FacilitiesType } from '@/types/supabase/facilities-type';
 import { Json } from '@/types/supabase/supabase-type';
-import Modal from '@/components/ui/hotel-room/Modal';
 import UpModal from '@/components/ui/hotel-up-image/Modal';
 import { UserType } from '@/types/supabase/user-type';
+import Navigation from './_components/Navigation';
 
 const HotelDetailPage = ({ params }: { params: { id: string } }) => {
   const hotelId = params?.id;
@@ -139,36 +138,7 @@ const HotelDetailPage = ({ params }: { params: { id: string } }) => {
       <div className="h-[60px]"></div>
 
       {/* 네비게이션 탭 */}
-      <div className="sticky top-0 z-10 bg-white border-b shadow-sm">
-        <div className="mx-[360px]">
-          <ul className="flex space-x-6 py-4">
-            {[
-              { id: 'overview', label: '개요' },
-              { id: 'rooms', label: '객실 선택' },
-              { id: 'reviews', label: '이용 후기' },
-              { id: 'services', label: '시설/서비스' },
-              { id: 'policies', label: '숙소 정책' },
-              { id: 'location', label: '위치' },
-              { id: 'nearby', label: '호텔 주변 명소' }
-            ].map((tab) => (
-              <li key={tab.id}>
-                <a
-                  href={`#${tab.id}`}
-                  className={`cursor-pointer pb-2 ${
-                    activeTab === tab.id ? 'border-b-2 border-[#A0522D] text-[#8B4513] font-semibold' : 'text-gray-800'
-                  }`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection(tab.id);
-                  }}
-                >
-                  {tab.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <Navigation activeTab={activeTab} scrollToSection={scrollToSection} />
 
       {/* 콘텐츠 영역 */}
       <div className="mx-[360px] py-6 space-y-16">
