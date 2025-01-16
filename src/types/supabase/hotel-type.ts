c94bc330ab093c3c0d373994fd8024118c59bb6d
Noon_sae <minseock7308@gmail.com>
2025-01-17 02:39:19 +0900

    📝 타입  추가 지정

--- src/types/supabase/hotel-type.ts
+++ src/types/supabase/hotel-type.ts
@@ -1,3 +1,13 @@
 import { Tables } from './supabase-type';
 
 export type HotelType = Tables<'hotels'>;
+
+type RoomWithPrice = {
+  price: number;
+};
+
+// 간소화된 호텔 타입 정의
+export interface HotelWithPriceOnly extends HotelType {
+  rooms: RoomWithPrice[];
+  min_price: number;
+}
