68eb5a5f54c7d4f640ea3229ccfeaeb2703dcd88
Noon_sae <minseock7308@gmail.com>
2025-01-17 10:26:00 +0900

    📝 화살표 위치값 수정

--- src/components/ui/slider/customArrow.tsx
+++ src/components/ui/slider/customArrow.tsx
@@ -5,7 +5,7 @@ const CustomPrevArrow = ({ onClick }: { onClick?: React.MouseEventHandler<HTMLBu
   return (
     <button
       onClick={onClick}
-      className="absolute top-[55%] -left-[30px] transform -translate-y-[100%] shadow-[0px_2px_4px_rgba(0,0,0,0.3)] rounded-full bg-white z-10"
+      className="absolute top-[55%] -left-[20px] transform -translate-y-[100%] shadow-[0px_2px_4px_rgba(0,0,0,0.3)] rounded-full bg-white z-10"
     >
       <FiChevronLeft className="w-[40px] h-[40px] text-[#777]" />
     </button>
@@ -17,7 +17,7 @@ const CustomNextArrow = ({ onClick }: { onClick?: React.MouseEventHandler<HTMLBu
   return (
     <button
       onClick={onClick}
-      className="absolute top-[55%] -right-[30px] transform -translate-y-[100%] shadow-[0px_2px_4px_rgba(0,0,0,0.3)] rounded-full bg-white z-10"
+      className="absolute top-[55%] -right-[20px] transform -translate-y-[100%] shadow-[0px_2px_4px_rgba(0,0,0,0.3)] rounded-full bg-white z-10"
     >
       <FiChevronRight className="w-[40px] h-[40px] text-[#777]" />
     </button>
