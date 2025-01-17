a51cc6888a90fcd9a8c0ab1c3968e4834fd10892
wooseok <youkn0wthat@naver.com>
2025-01-17 17:15:53 +0900

    📝 리뷰 모달에 기능추가 (후기 종류 , 평점 차순)

--- src/components/ui/hotel-review/ReviewsModal.tsx
+++ src/components/ui/hotel-review/ReviewsModal.tsx
@@ -1,4 +1,5 @@
 import { ReviewType } from '@/types/supabase/review-type';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const LikeIcon = () => {
@@ -21,17 +22,19 @@ const ReviewsModal = ({
   onClose: () => void;
   reviews: ReviewType[];
 }) => {
-  if (!isOpen) return null;
-
   const [averageRating, setAverageRating] = useState<number>(0);
   const [reviewCount, setReviewCount] = useState<number>(0);
-  const [ratingCounts, setRatingCounts] = useState<{ [key: number]: number }>({
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0
-  });
+  const [ratingCounts, setRatingCounts] = useState<{ [key: number]: number }>({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 });
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [sortOption, setSortOption] = useState('최신순');
+  const [filterOption, setFilterOption] = useState('전체 후기'); // "전체 후기" 또는 "사진 후기"
+  const [sortedReviews, setSortedReviews] = useState<ReviewType[]>([]);
+
+  const handleSelectOption = (option: string) => {
+    setSortOption(option);
+    setIsDropdownOpen(false);
+  };
 
   const renderStars = (rating: number) => {
     return Array.from({ length: 5 }).map((_, index) => (
@@ -43,54 +46,95 @@ const ReviewsModal = ({
 
   useEffect(() => {
     if (reviews && reviews.length > 0) {
-      const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0); // 모든 리뷰의 평점 합
-      setAverageRating(totalRating / reviews.length); // 평균 평점 계산
-      setReviewCount(reviews.length); // 리뷰 수 설정
+      let filteredReviews = [...reviews];
 
-      // 평점별 리뷰 수 카운트
+      // 필터 옵션에 따른 필터링
+      if (filterOption === '사진 후기') {
+        filteredReviews = filteredReviews.filter(
+          (review) => Array.isArray(review.review_img_url) && review.review_img_url.length > 0
+        );
+      }
+
+      // 정렬 옵션에 따른 정렬
+      if (sortOption === '최신순') {
+        filteredReviews.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+      } else if (sortOption === '별점 높으순') {
+        filteredReviews.sort((a, b) => b.rating - a.rating);
+      } else if (sortOption === '별점 낮으순') {
+        filteredReviews.sort((a, b) => a.rating - b.rating);
+      }
+
+      setSortedReviews(filteredReviews);
+
+      // 통계 계산
+      const totalRating = filteredReviews.reduce((sum, review) => sum + review.rating, 0);
+      setAverageRating(filteredReviews.length > 0 ? totalRating / filteredReviews.length : 0);
+      setReviewCount(filteredReviews.length);
+
       const newRatingCounts: { [key: number]: number } = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-      reviews.forEach((review) => {
+      filteredReviews.forEach((review) => {
         if (newRatingCounts[review.rating] !== undefined) {
           newRatingCounts[review.rating] += 1;
         }
       });
       setRatingCounts(newRatingCounts);
+    } else {
+      setSortedReviews([]);
+      setAverageRating(0);
+      setReviewCount(0);
+      setRatingCounts({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 });
     }
-  }, [reviews]);
+  }, [reviews, sortOption, filterOption]);
+
+  if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
-      <div
-        className="bg-white shadow-lg p-6 rounded-lg overflow-y-auto"
-        style={{
-          width: '600px',
-          height: '740px',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)'
-        }}
-      >
-        <button className="absolute top-4 right-4 text-gray-500 hover:text-black" onClick={onClose}>
-          닫기
-        </button>
-        <h2 className="text-xl font-bold mb-4">전체 이용 후기</h2>
+      <div className="bg-white shadow-lg p-6 rounded-lg overflow-y-auto scrollbar-hide w-[600px] h-[740px]">
+        {/* 상단 헤더 */}
+        <div className="flex justify-between items-center h-[67px] mb-4 bg-[#221A1A] -mx-6 -mt-6 px-6 py-2 rounded-t-lg">
+          <h1 className="text-white font-semibold">이용후기</h1>
+          <button onClick={onClose} className="text-gray-300 hover:text-white px-2 py-1 rounded-md">
+            닫기
+          </button>
+        </div>
 
-        {/* 상단 별점, 리뷰 수 */}
+        {/* 필터 버튼 */}
+        <div className="text-base text-[#534431] flex gap-5 mb-4">
+          <h2
+            onClick={() => setFilterOption('전체 후기')}
+            className={`cursor-pointer px-4 py-2 rounded-lg font-bold ${
+              filterOption === '전체 후기' ? ' text-[#534431]' : ' text-[#777]'
+            }`}
+          >
+            전체 후기
+          </h2>
+          <h2
+            onClick={() => setFilterOption('사진 후기')}
+            className={`cursor-pointer px-4 py-2 rounded-lg font-bold ${
+              filterOption === '사진 후기' ? ' text-[#534431]' : ' text-[#777]'
+            }`}
+          >
+            사진 후기
+          </h2>
+        </div>
+        <div className="border-b border-gray-200"></div>
+
+        {/* 별점, 리뷰 수 */}
         <div className="flex justify-between items-center mb-4">
-          <div className="flex items-center">
-            <div>
-              <span className="text-4xl font-bold">{LikeIcon()}</span>
-              <span className="text-sm text-gray-500 ml-2">({reviewCount}개의 리뷰)</span>
+          <div className="flex items-center gap-4">
+            <div className="flex justify-center items-center flex-col ml-[60px]">
+              <span className="text-4xl font-black">{LikeIcon()}</span>
+              <span className="text-lg text-gray-500 ml-2 mt-1">총 {reviewCount}개의 평가</span>
             </div>
-            <span className="text-3xl">{averageRating.toFixed(1)}</span>
+            <span className="text-5xl font-extrabold -mt-6 text-[#232527]">{averageRating.toFixed(1)}</span>
           </div>
-          <div className="mb-4">
+          <div>
             {[5, 4, 3, 2, 1].map((rating) => (
               <div key={rating} className="flex justify-between items-center text-sm text-gray-500 mb-2">
                 <span>{rating}점</span>
                 <div className="flex items-center">
-                  <div className="bg-[#B3916A] rounded-full w-[250px] h-2 mr-2"></div>
+                  <div className="bg-[#B3916A] rounded-full w-[180px] h-2 mr-2"></div>
                   <span>{ratingCounts[rating]}</span>
                 </div>
               </div>
@@ -98,43 +142,79 @@ const ReviewsModal = ({
           </div>
         </div>
 
-        {/* 평점별 리뷰 수 그래프 */}
+        {/* 정렬 드롭다운 */}
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="font-bold text-[#444]">총 {reviewCount}개의 후기</h2>
 
-        <button className="text-sm text-gray-600 border border-gray-400 px-2 py-1 ml-[380px] mb- w-[160px] h-[48px] rounded-md hover:bg-gray-200">
-          추천순
-        </button>
+          <div className="relative">
+            <button
+              className="text-sm text-gray-600 border border-gray-400 px-2 py-1 w-[150px] h-[48px] rounded-md hover:bg-gray-200 flex items-center justify-between"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              {sortOption}
+              <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+              </svg>
+            </button>
 
-        {/* 리뷰 목록 */}
-        <div className="flex flex-col gap-4 overflow-y-auto max-h-[500px]">
-          {reviews.length > 0 ? (
-            reviews.map((review: any) => (
-              <div key={review.id} className="p-4 border rounded-lg shadow-md bg-gray-50">
-                {/* 프로필, 이름, 작성일 */}
-                <div className="flex gap-4 mb-2">
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-[160px] bg-white border border-gray-300 rounded-md shadow-md z-10">
+                <button
+                  onClick={() => handleSelectOption('최신순')}
+                  className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                >
+                  최신순
+                </button>
+                <button
+                  onClick={() => handleSelectOption('별점 높으순')}
+                  className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                >
+                  별점 높으순
+                </button>
+                <button
+                  onClick={() => handleSelectOption('별점 낮으순')}
+                  className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                >
+                  별점 낮으순
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* 리뷰 리스트 */}
+        <div className="flex flex-col gap-10">
+          {sortedReviews.length > 0 ? (
+            sortedReviews.map((review) => (
+              <div key={review.id} className="p-4 border-b border-gray-200">
+                <div className="flex gap-4 mb-2 justify-center items-center">
                   <img
-                    src={review.users?.profile_img || '/placeholder-profile.png'} // 프로필 이미지
+                    src={review.users?.profile_img || '/placeholder-profile.png'}
                     alt="Profile"
                     className="w-[50px] h-[50px] rounded-full object-cover"
                   />
                   <div className="flex justify-between w-full">
-                    <p className="font-bold text-lg">{review.users?.nickname || '익명'}</p>
+                    <p className="font-bold text-[#444] text-lg">{review.users?.nickname || '익명'}</p>
                     <p className="text-sm text-gray-500">작성일: {new Date(review.created_at).toLocaleDateString()}</p>
                   </div>
                 </div>
 
-                {/* 별점 표시 */}
                 <p>{renderStars(review.rating)}</p>
 
-                <p className="text-sm text-gray-700 mb-2">{review.comment}</p>
-
-                {/* 사진 리뷰 */}
-                {review.review_img_url?.length > 0 && (
-                  <div className="flex gap-2 mt-2">
-                    {review.review_img_url.map((url: string, index: number) => (
-                      <img key={index} src={url} alt="Review Image" className="w-24 h-24 object-cover rounded-md" />
-                    ))}
-                  </div>
-                )}
+                {Array.isArray(review.review_img_url) &&
+                  review.review_img_url.map((item, idx) => (
+                    <Image
+                      key={idx}
+                      src={item}
+                      alt="Review Image"
+                      width={300}
+                      height={300}
+                      className="inline-block w-24 h-24 object-cover rounded-md mr-2"
+                    />
+                  ))}
+                <div className="mt-4">
+                  <p className="text-sm text-gray-700 mb-2">{review.comment}</p>
+                </div>
               </div>
             ))
           ) : (
