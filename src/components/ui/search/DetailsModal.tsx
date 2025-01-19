7520108e678d61db77e67a69a2ace0cce6e0deaa
Noon_sae <minseock7308@gmail.com>
2025-01-20 06:21:18 +0900

    📝 입력값 초기화 버튼 및 기능 추가

--- src/components/ui/search/DetailsModal.tsx
+++ src/components/ui/search/DetailsModal.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import { IoIosAddCircleOutline, IoIosRemoveCircleOutline } from 'react-icons/io';
 import useSearchStore from '@/store/useSearchStore'; // zustand store import
 
-const DetailsModal = ({ right = '360px', top } : {right?: string; top? :string; }) => {
+const DetailsModal = ({ right = '360px', top }: { right?: string; top?: string }) => {
+  
   const { setDetails } = useSearchStore(); // zustand의 setDetails 사용
-  const [filters, setFilters] = React.useState({
+  
+  const initialFilters = {
     객실수: 1,
     성인: 1,
     어린이: 0,
     반려동물: 0
-  });
+  };
+
+  const [filters, setFilters] = React.useState(initialFilters);  
 
   const handleChange = (type: keyof typeof filters, increment: boolean) => {
     setFilters((prev) => ({
@@ -41,10 +45,14 @@ const DetailsModal = ({ right = '360px', top } : {right?: string; top? :string; 
     setDetails(formattedDetails); // zustand에 저장
   };
 
+  const handleResetFilters = () => {
+    setFilters(initialFilters); // 초기 상태로 되돌리기
+  };
+
   return (
     <div
       style={{ right, top }}
-      className="fixed bg-white p-6 rounded-[12px] shadow-lg w-[432px] h-[364px] z-50"
+      className="fixed w-[432px] h-[364px] px-8 py-6 bg-white rounded-[12px] shadow-lg z-50"
       onClick={(e) => e.stopPropagation()} // 내부 클릭 방지
     >
       {Object.keys(filters).map((key) => (
@@ -73,15 +81,20 @@ const DetailsModal = ({ right = '360px', top } : {right?: string; top? :string; 
           </p>
         </div>
       ))}
-      {/* 적용 버튼 */}
-      <div className="w-full flex justify-end">
+      {/* 버튼 영역 */}
+      <div className="w-full flex justify-evenly mt-8">
         <button
-          onClick={() => {
-            handleSaveDetails(); // zustand에 저장
-          }}
-          className="w-[124px] h-[44px] mt-4 bg-[#B3916A] text-white py-2 rounded"
+          onClick={handleSaveDetails} // zustand에 저장
+          className="w-[124px] h-[44px] px-6 py-[10px] bg-[#B3916A] text-white text-[18px] rounded-[8px] hover:bg-[#8F7455] active:bg-[#6B573F]"
         >
           적용하기
+        </button>
+
+        <button
+          onClick={handleResetFilters} // 상태값 초기화
+          className="w-[124px] h-[44px] px-6 py-[10px] bg-[#efefef] text-gray-800 rounded-[8px] border border-gray-200 hover:bg-[#bfbfbf] active:bg-[#a0a0a0]"
+        >
+          초기화
         </button>
       </div>
     </div>
