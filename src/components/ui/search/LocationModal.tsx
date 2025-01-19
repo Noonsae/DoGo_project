ed8dbbbbe8abba7159c2ef4e1adc5da5cbe96c5d
Noon_sae <minseock7308@gmail.com>
2025-01-19 18:34:14 +0900

    📝 역순 정렬 및 item 가공 로직 추가 및 주석 정리

--- src/components/ui/search/LocationModal.tsx
+++ src/components/ui/search/LocationModal.tsx
@@ -12,36 +12,42 @@ import { IoClose } from 'react-icons/io5';
 const LocationModal = ({ onSelectLocation }: { onSelectLocation: (label: string) => void }) => {
   const history = useHistoryStore((state) => state.history);
   const removeHotel = useHistoryStore((state) => state.removeHotel);
-  
-  const [selectedLocal, setSelectedLocal] = useState<string>('');
+  const [selectedLocal, setSelectedLocal] = useState<string>(''); // input으로 넘겨주는 상태값
 
   const getLocationLabel = (locationId: string) => {
     const location = locations.find((loc) => loc.id === locationId);
     return location ? location.label : '알 수 없음';
   };
 
-  const uniqueHistory = history.filter(
-    (item, index, self) => index === self.findIndex((h) => h.location === item.location)
-  );
+  // 1. 역순으로 정렬한 뒤,
+  const reversedHistory = [...history].reverse();
 
+  // 2. 중복 값을 제거하고 최대 3개의 리스트만 반환
+  const uniqueHistory = reversedHistory
+    .filter((item, index, self) => index === self.findIndex((h) => h.location === item.location))
+    .slice(0, 3); // 최대 3개만 유지
+
+  // 전체를 제외한 location constant의 값을 필터링
   const filteredLocations = locations.filter((location) => location.id !== 'all');
 
+  // 선택한 지역값을 input의 value로 넘겨줌
   const handleSelectHistory = (locationId: string) => {
     const label = getLocationLabel(locationId); // locationId로 label 가져오기
     setSelectedLocal(label); // 선택된 label 상태 업데이트
     onSelectLocation(label); // 부모 컴포넌트에 전달
   };
 
-  const handleLocationClick = (label: string) => {    
+  // 선택한 버튼의 지역값을 input의 value로 넘겨줌
+  const handleLocationClick = (label: string) => {
     setSelectedLocal(label); // 선택된 버튼의 label을 저장
     onSelectLocation(label); // 부모 컴포넌트로 선택된 label 전달
   };
 
   return (
-    <div className="fixed left-[3%] top-[180px] w-[404px] px-9 py-8  bg-white shadow-[0px_4px_12px_rgba(0,0,0,0.1)] rounded-[12px] z-50">
+    <div className="fixed left-[17%] top-[180px] w-[404px] px-9 py-8 bg-white shadow-[0px_4px_12px_rgba(0,0,0,0.1)] rounded-[12px] z-50">
       <div>
         <p className="mb-3 text-[20px] text-[#232527] font-semibold">최근 검색지</p>
-        <div className="h-[165px] overflow-auto">
+        <div className="h-[126px] overflow-hidden">
           {uniqueHistory.map((history) => (
             <div
               className="h-[42px] px-1 py-2 flex flex-row items-center justify-between rounded-[8px] cursor-pointer hover:bg-[#f5f5f5]"
@@ -61,7 +67,7 @@ const LocationModal = ({ onSelectLocation }: { onSelectLocation: (label: string)
           ))}
         </div>
       </div>
-      <div className="">
+      <div className="mt-8">
         <p className="mb-3 text-[20px] text-[#232527] font-semibold">지역</p>
         <div className="flex flex-row flex-wrap gap-3">
           {filteredLocations.map((location) => (
