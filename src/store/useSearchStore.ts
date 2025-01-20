c236431684106a3889f6606e4f46557718d28fec
Noon_sae <minseock7308@gmail.com>
2025-01-20 09:12:07 +0900

    📝 스케쥴 상태 추가

--- src/store/useSearchStore.ts
+++ src/store/useSearchStore.ts
@@ -7,10 +7,12 @@ const useSearchStore = create<SearchState>((set) => ({
   checkIn: '',
   checkOut: '',
   details: '',
+  schedule: '',
   setLocation: (location) => set({ location }),
   setCheckIn: (checkIn) => set({ checkIn }),
   setCheckOut: (checkOut) => set({ checkOut }),
-  setDetails: (details) => set({ details })
+  setDetails: (details) => set({ details }),
+  setSchedule: (schedule) => set({ schedule })
 }));
 
 export default useSearchStore;
