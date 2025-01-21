d552bb57ef9d4c08e7640abd666667356f5fa89b
Noon_sae <minseock7308@gmail.com>
2025-01-21 16:01:18 +0900

    📝 구조 개선

--- src/app/hotel-list/_components/RenderStars.tsx
+++ src/app/hotel-list/_components/RenderStars.tsx
@@ -1,12 +1,12 @@
 const RenderStars = ({ rating }: { rating: number }) => {
   return (
-    <>
+    <div>
       {Array.from({ length: 5 }).map((_, index) => (
-        <span key={index} className={`text-lg ${index < rating ? 'text-yellow-500' : 'text-gray-300'}`}>
+        <span key={index} className={`text-lg ${index < rating ? 'text-[#EEC18D]' : 'text-gray-300'}`}>
           ★
         </span>
       ))}
-    </>
+    </div>
   );
 };
 
