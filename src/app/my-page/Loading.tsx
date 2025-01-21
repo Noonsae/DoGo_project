a97e6ee9335cf307297ea32e49b62c051b3e118f
Noon_sae <minseock7308@gmail.com>
2025-01-21 16:01:32 +0900

    📝 마이페이지 임시 로딩페이지 구현

---
+++ src/app/my-page/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <p className="text-[32px] font-medium">페이지가 준비중입니다. 잠시만 기다려주세요.</p>
+    </div>
+  );
+};
+
+export default Loading
