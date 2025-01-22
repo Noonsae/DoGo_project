18692ddcffb26ac9dfa77c44ebe4c37e0554c6e1
wooseok <youkn0wthat@naver.com>
2025-01-22 19:24:19 +0900

    💄 react-icons 에서 svg 아이콘으로 변경

--- src/app/hotel-list/_components/HotelsCardList.tsx
+++ src/app/hotel-list/_components/HotelsCardList.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import useHotelRooms from '@/hooks/room/useHotelRooms';
 import useSerViceFacility from '@/hooks/serviceFacility/useServiceFacility';
 import RenderStars from './RenderStars';
-import { RiThumbUpFill } from 'react-icons/ri';
 
 interface HotelListItemProps {
   hotel: HotelType & { min_price?: number | null };
@@ -32,6 +31,17 @@ const HotelCardList = ({ hotel, isFavorite, hotelId }: HotelListItemProps) => {
 
   const translateView = (view: string): string => {
     return viewTranslationMap[view] || '알 수 없는 뷰';
+  };
+
+  const ratingIcon = () => {
+    return (
+      <svg width="77" height="25" viewBox="0 0 77 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M2 9.49997H5V21.5H2C1.44772 21.5 1 21.0523 1 20.5V10.5C1 9.94769 1.44772 9.49997 2 9.49997ZM7.29289 8.20708L13.6934 1.80661C13.8693 1.63066 14.1479 1.61087 14.3469 1.76016L15.1995 2.3996C15.6842 2.76312 15.9026 3.38253 15.7531 3.96966L14.5998 8.49997H21C22.1046 8.49997 23 9.3954 23 10.5V12.6043C23 12.8656 22.9488 13.1243 22.8494 13.3658L19.755 20.8807C19.6007 21.2554 19.2355 21.5 18.8303 21.5H8C7.44772 21.5 7 21.0523 7 20.5V8.91419C7 8.64897 7.10536 8.39462 7.29289 8.20708Z"
+          fill="#EEC18D"
+        />
+      </svg>
+    );
   };
 
   const totalReviews = allReviews.length;
@@ -77,9 +87,10 @@ const HotelCardList = ({ hotel, isFavorite, hotelId }: HotelListItemProps) => {
           {/* 리뷰 */}
           {!loading && (
             <div className="flex flex-row items-center">
-              <RiThumbUpFill className="w-6 h-6 text-[#EEC18D]" />
-              <p className="ml-1 text-[18px] font-semibold">4.8</p>
-              <span className="ml-2 text-[#A0A0A0]">(3,222)</span>
+              <p className="w-6 h-6 text-[#EEC18D]">{ratingIcon()}</p>
+
+              <p className="ml-1 text-[18px] font-semibold">35</p>
+              <span className="ml-2 text-[#A0A0A0]">(35)</span>
             </div>
           )}
         </div>
