01055c74a62752330c81af8bc1f4afb13aed88d7
Noon_sae <minseock7308@gmail.com>
2025-01-22 18:38:38 +0900

    📝 사용했던 아이콘의 svg파일 생성

---
+++ src/components/ui/icon/FiChevronRightIcon.tsx
@@ -0,0 +1,17 @@
+import { IconType } from '@/types/icon/icon-type';
+
+const FiChevronRightIcon: React.FC<IconType> = (className) => {
+  return (
+    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9 18.5L15 12.5L9 6.5"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default FiChevronRightIcon;
