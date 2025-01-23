d2d34c00089801311cf86911061ec9f58b947e11
hansolChoi29 <ggksthf29@gmail.com>
2025-01-24 00:59:59 +0900

    ✏️ sign-up 모두 반응형 완료

--- src/app/(auth)/sign-in/_components/KakaoSignIn.tsx
+++ src/app/(auth)/sign-in/_components/KakaoSignIn.tsx
@@ -14,15 +14,13 @@ const KakaoSignIn = () => {
   };
 
   return (
-    <div className="flex flex-col items-center px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col items-center ">
       <button
         onClick={kakaoLogin}
-        className="w-[400px] bg-[#FEE500] text-black py-1 rounded-[8px] items-center gap-2 hover:text-gray-500 transition"
+        className="w-full max-w-[450px] bg-[#FEE500] text-black py-1 rounded-[8px] flex items-center gap-2 hover:text-gray-500 transition"
       >
-        <div className="flex items-center">
-          <KaKaoIcon />
-          <p className="w-full">카카오로 시작하기</p>
-        </div>
+        <KaKaoIcon />
+        <p className="w-full text-center">카카오로 시작하기</p>
       </button>
     </div>
   );
