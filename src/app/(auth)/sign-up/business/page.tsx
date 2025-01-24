83faa7ed96c22df49c87cc98bcd172a417d90aec
hansolChoi29 <ggksthf29@gmail.com>
2025-01-24 17:17:52 +0900

    ✏️ business 모달 정리 및 에러메시지 재설정

--- src/app/(auth)/sign-up/business/page.tsx
+++ src/app/(auth)/sign-up/business/page.tsx
@@ -16,14 +16,9 @@ export default function SignUpBusinessPage() {
   const [nickname, setNickname] = useState('');
   const [businessNumber, setBusinessNumber] = useState('');
   const [isModlaOpen, setIsModalOpen] = useState(false);
-  // const setUser = useAuthStore((state) => state.setUser);
-
-  const router = useRouter();
 
   const handleSignup = async () => {
     try {
-      // const supabase = browserSupabase(); // 클라이언트 생성
-
       const result = await handleSignupAction({
         email,
         password,
@@ -38,7 +33,7 @@ export default function SignUpBusinessPage() {
         await Swal.fire({
           icon: 'error',
           title: '회원가입 실패',
-          text: result.message
+          text: '모두 작성해주세요!'
         });
         return;
       }
@@ -47,43 +42,15 @@ export default function SignUpBusinessPage() {
         title: '회원가입 성공',
         text: `${name}님, 회원가입이 완료되었습니다!`
       });
-      // if (!result.success) {
-      //   setError(result.message);
-      //   return;
-      // }
-      // 서버액션을 쓰면 signInWithPassword 함수 안써도 돼서 지움
-      // 자동 로그인
-      // const { error: loginError } = await supabase.auth.signInWithPassword({
-      //   email,
-      //   password
-      // });
-
-      // if (loginError) {
-      //   setError('로그인 중 오류가 발생했습니다.');
-      //   return;
-      // }
-
-      // store에 유저 정보 저장
-      // setUser({
-      //   email,
-      //   name,
-      //   phone,
-      //   role: 'business'
-      // });
-      // setIsModalOpen(true);
       window.location.href = '/';
-
-      // 모달을 살리고 싶은데, 뭘해도 안됨.⭐
-      // setTimeout(setIsModalOpen, 2000);
     } catch (err: any) {
       setError('회원가입 중 오류가 발생했습니다.');
       console.error(err);
     }
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
-    // window.location.href = '/';
-    // router.push('/');
   };
 
   return (
@@ -105,24 +72,6 @@ export default function SignUpBusinessPage() {
         setError={setError}
         handleSignup={handleSignup}
       />
-      {/* 모달추가! */}
-      <SignupModal isOpen={isModlaOpen} onClose={closeModal}>
-        <div className="flex flex-col p-[40px 32px 32px 32px] items-center g-[32px]">
-          <div className="m-[32px]">
-            <img src="/images/clap.png" alt="clap" width={100} height={100} />
-          </div>
-          <div className="text-[28px] text-[#444] aligin-center">
-            <p className="flex justify-center flex-row">환영합니다!</p>
-            <p className="flex justify-center flex-row">회원가입이 완료되었습니다.</p>
-          </div>
-          <button
-            className="bg-[#B3916A] m-[32px] text-white items-center p-[8px 24px] w-[436px] h-[48px] rounded-[8px]"
-            onClick={closeModal}
-          >
-            홈페이지로 이동하기
-          </button>
-        </div>
-      </SignupModal>
     </div>
   );
 }
