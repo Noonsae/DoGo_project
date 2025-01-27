7c4e212dbe1f29e56096d96ca47f39812810a43d
hansolChoi29 <ggksthf29@gmail.com>
2025-01-28 01:44:05 +0900

    ✏️ style 수정 -설화님-user,business 여백

--- src/app/(auth)/sign-in/_components/KakaoSignIn.tsx
+++ src/app/(auth)/sign-in/_components/KakaoSignIn.tsx
@@ -15,7 +15,7 @@ const KakaoSignIn = () => {
     <div className="flex flex-col items-center">
       <button
         onClick={kakaoLogin}
-        className="w-full max-w-[450px] bg-[#FEE500] text-black py-1 rounded-[8px] flex items-center gap-2 hover:text-gray-500 transition relative"
+        className="w-full max-w-[450px] h-[48px] sm:h-[56px] bg-[#FEE500] text-black py-1 rounded-[8px] flex items-center gap-2 hover:text-gray-500 transition relative"
       >
         <KaKaoIcon />
         <p className="absolute inset-0 flex items-center justify-center">카카오로 시작하기</p>
