b22fd8bc85dba002ce5e98d170b65152cdcb6d97
hansolChoi29 <ggksthf29@gmail.com>
2025-01-27 15:53:12 +0900

    🐛 가입,로그인 둘 다 로고 떠라!!!

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -48,9 +48,7 @@ const SignInPage = () => {
         });
         return;
       }
-      // commit을 위한 주석
-      // commit을 위한 주석
-      // commit을 위한 주석
+
       await Swal.fire({
         icon: 'success',
         title: '로그인 성공',
@@ -72,7 +70,7 @@ const SignInPage = () => {
     <div className="flex justify-center items-center min-h-screen px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-[400px] lg:h-auto">
         <div className="mt-[94px] mb-[40px] flex flex-col justify-center items-center">
-          <Image src="/images/DoGo.png" alt="Dogo" priority width={140.6} height={39.9} />
+          <Image src="/images/DoGo.png" alt="Dogo" priority width={140.6} height={39.9} unoptimized />
         </div>
         <div className="flex justify-between mb-[40px] border-b-2">
           <button
