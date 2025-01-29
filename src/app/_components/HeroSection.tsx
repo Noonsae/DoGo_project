f1bb7cdd3b8c412f6d75df9766c97f478158ce25
Noon_sae <minseock7308@gmail.com>
2025-01-29 21:35:40 +0900

    📝 title, subTitle 멘트 수정

--- src/app/_components/HeroSection.tsx
+++ src/app/_components/HeroSection.tsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     <section className="w-full h-[520px] bg-[#221A1A]">
       <div className="max-w-[1300px] h-full  px-[50px] mx-auto pt-[180px] text-center text-white">
-        <h2 className="mb-[12px] text-[40px] font-semibold ">TITLE</h2>
-        <h3 className="text-[24px] font-normal leading-[1.45]">sub title</h3>
+        <h2 className="mb-[12px] text-[40px] font-medium ">여행의 시작, DoGo</h2>
+        <h3 className="text-[24px] font-normal leading-[1.45]">특별한 하루를 위한 최고의 선택</h3>
       </div>
       <SearchBox />
     </section>
