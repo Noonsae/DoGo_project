df6b2b7dfb8345f2d8c756483482b231ac7c4a51
Noon_sae <minseock7308@gmail.com>
2025-01-30 01:35:31 +0900

    📝 검색 기록이 없을 경우 text 추가

--- src/components/ui/search/LocationModal.tsx
+++ src/components/ui/search/LocationModal.tsx
@@ -9,17 +9,11 @@ import IoMdPinIcon from '../icon/IoMdPinIcon';
 import IoCloseIcon from '../icon/IoCloseIcon';
 import useSearchStore from '@/store/useSearchStore';
 
-const LocationModal = ({  
-  top,
-  left,
-}: {  
-  top?: string;
-  left?: string;
-}) => {  
+const LocationModal = ({ top, left }: { top?: string; left?: string }) => {
   const history = useSearchHistoryStore((state) => state.history); // 히스토리 값 가져오기
   const removeHistory = useSearchHistoryStore((state) => state.removeHistory);
   const [selectLabel, setSelectLabel] = useState('');
-  const { location, setLocation } = useSearchStore();  
+  const { location, setLocation } = useSearchStore();
 
   const getLocationLabel = (locationId: string) => {
     const location = locations.find((loc) => loc.id === locationId);
@@ -48,30 +42,35 @@ const LocationModal = ({
       <div>
         <p className="mb-3 text-[20px] text-[#232527] font-semibold">최근 검색지</p>
         <div className="h-[126px] overflow-hidden">
-          {history.map((item, index) => (
-            <div
-              key={index}
-              className="h-[42px] px-1 py-2 flex flex-row items-center justify-between rounded-[8px] cursor-pointer hover:bg-[#f5f5f5]"
-              onClick={() => handleLocationClick(item)}
-            >
-              <div className="flex flex-row items-center justify-start">
-                <IoMdPinIcon className="w-6 h-6 text-[#777] mr-2" />
-                <p className="p-2 text-[18px] text-[#636363] leading-[1.45] ">{item}</p>
-              </div>
-              <button
-                type="button"
-                
-                onClick={(e) => {
-                  e.stopPropagation(); // 이벤트 버블링 방지
-                  handleRemoveSearchHistory(item);
-                }} // 삭제 버튼 동작
+          {history.length > 0 ? (
+            history.map((item, index) => (
+              <div
+                key={index}
+                className="h-[42px] px-1 py-2 flex flex-row items-center justify-between rounded-[8px] cursor-pointer hover:bg-[#f5f5f5]"
+                onClick={() => handleLocationClick(item)}
               >
-                <div>
-                  <IoCloseIcon className="w-6 h-6 text-[#777]" />
+                <div className="flex flex-row items-center justify-start">
+                  <IoMdPinIcon className="w-6 h-6 text-[#777] mr-2" />
+                  <p className="p-2 text-[18px] text-[#636363] leading-[1.45] ">{item}</p>
                 </div>
-              </button>
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation(); // 이벤트 버블링 방지
+                    handleRemoveSearchHistory(item);
+                  }} // 삭제 버튼 동작
+                >
+                  <div>
+                    <IoCloseIcon className="w-6 h-6 text-[#777]" />
+                  </div>
+                </button>
+              </div>
+            ))
+          ) : (
+            <div className="w-full h-[126px] flex flex-row items-center justify-center">
+              <p className="text-[18px] text-[#636363] font-normal leading-[1.45]">이전 검색 기록을 찾을 수 없습니다.</p>
             </div>
-          ))}
+          )}
         </div>
       </div>
       <div className="mt-8">
