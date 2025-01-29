b97265475c7daa1dcccb887d32b795489d4e0afc
Noon_sae <minseock7308@gmail.com>
2025-01-29 21:07:50 +0900

    🚧 search 기록 로직 주스탠드 제거 (잘못된 로직 수정 작업 진행 중)

--- src/components/ui/search/LocationModal.tsx
+++ src/components/ui/search/LocationModal.tsx
@@ -2,29 +2,27 @@
 
 import { useState } from 'react';
 
-import useHistoryStore from '@/store/useHistoryStore';
-
 import { locations } from '@/constants/constant';
 import IoMdPinIcon from '../icon/IoMdPinIcon';
 import IoCloseIcon from '../icon/IoCloseIcon';
 
-const LocationModal = ({ onSelectLocation, top, left }: { onSelectLocation: (label: string) => void; top?: string; left?: string; }) => {
-  const history = useHistoryStore((state) => state.history);
-  const removeHotel = useHistoryStore((state) => state.removeHotel);
+const LocationModal = ({
+  onSelectLocation,
+  top,
+  left,
+  searchHistory
+}: {
+  onSelectLocation: (label: string) => void;
+  top?: string;
+  left?: string;
+  searchHistory?: string[]
+}) => {
   const [selectedLocal, setSelectedLocal] = useState<string>(''); // input으로 넘겨주는 상태값
 
   const getLocationLabel = (locationId: string) => {
     const location = locations.find((loc) => loc.id === locationId);
     return location ? location.label : '알 수 없음';
   };
-
-  // 1. 역순으로 정렬한 뒤,
-  const reversedHistory = [...history].reverse();
-
-  // 2. 중복 값을 제거하고 최대 3개의 리스트만 반환
-  const uniqueHistory = reversedHistory
-    .filter((item, index, self) => index === self.findIndex((h) => h.location === item.location))
-    .slice(0, 3); // 최대 3개만 유지
 
   // 전체를 제외한 location constant의 값을 필터링
   const filteredLocations = locations.filter((location) => location.id !== 'all');
@@ -50,18 +48,18 @@ const LocationModal = ({ onSelectLocation, top, left }: { onSelectLocation: (lab
       <div>
         <p className="mb-3 text-[20px] text-[#232527] font-semibold">최근 검색지</p>
         <div className="h-[126px] overflow-hidden">
-          {uniqueHistory.map((history) => (
+          {[].map((history) => (
             <div
               className="h-[42px] px-1 py-2 flex flex-row items-center justify-between rounded-[8px] cursor-pointer hover:bg-[#f5f5f5]"
-              onClick={() => handleSelectHistory(history.location)}
+              onClick={() => handleSelectHistory(history)}
             >
               <div className="flex flex-row items-center justify-start">
                 <IoMdPinIcon className="w-6 h-6 text-[#777] mr-2" />
-                <p className="p-2 text-[18px] text-[#636363] leading-[1.45] ">{getLocationLabel(history.location)}</p>
+                <p className="p-2 text-[18px] text-[#636363] leading-[1.45] ">{getLocationLabel(history)}</p>
               </div>
               <button
                 type="button"
-                onClick={() => removeHotel(history.location)} // 삭제 버튼 동작
+                // onClick={() => removeHotel(history)} // 삭제 버튼 동작
               >
                 <div>
                   <IoCloseIcon className="w-6 h-6 text-[#777]" />
