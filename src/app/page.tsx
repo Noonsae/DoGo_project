bda7f93fa74ca97f67e33f6d029e7868b04f0ba9
Noon_sae <minseock7308@gmail.com>
2025-01-30 12:00:36 +0900

    📝 미리보기 사진 주소 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,10 @@ import EventSection from './_components/EventSection';
 import HeroSection from './_components/HeroSection';
 import ContentsList from './_components/ContentsList';
 
-const siteUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'; // 로컬 개발 환경 대비
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL; // 로컬 개발 환경 대비
 
 export const metadata: Metadata = {
+  
   title: 'DoGo : 홈페이지',
   description: '국내 4성~5성 호텔 예약 애플리케이션 DoGo의 홈페이지입니다.',
   openGraph: {
@@ -18,13 +17,14 @@ export const metadata: Metadata = {
     siteName: 'DoGo',
     images: [
       {
-        url: `${siteUrl}/images/DoGo_site_preview.webp`, // OpenGraph 이미지 URL
+        url: `${siteUrl}/images/DoGo_site_preview.webp`, // OpenGraph 미리보기 이미지 URL
         width: 1200,
         height: 630,
         alt: 'DoGo: 호텔 예약 애플리케이션'
       }
-    ]
-  }
+    ],
+    type: 'website' // 사이트 타입 명시
+  },
 };
 
 const HomePage = () => {
