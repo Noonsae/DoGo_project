7f8116df433f9d76634d5f86cbacb2256e3d387e
Noon_sae <minseock7308@gmail.com>
2025-02-04 17:58:45 +0900

    📝 예약 페이지 코드 임시 정리

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -3,17 +3,17 @@
 import { useState } from 'react';
 
 import { useSearchParams } from 'next/navigation';
-import { countryCodes } from '@/constants/constant';
-
-import Sidebar from './_components/Sidebar';
 
 import useAuthStore from '@/store/useAuth';
 import useUserQuery from '@/hooks/user/useUserData';
-import CheckInInfo from './_components/CheckInInfo';
+
+import Sidebar from './_components/Sidebar';
 import GuestInfo from './_components/GuestInfo';
 import ContactInfo from './_components/ContactInfo';
 import Requests from './_components/Requests';
 import AgreementAndPayment from './_components/AgreementAndPayment ';
+
+import { countryCodes } from '@/constants/constant';
 
 const Booking = () => {
   const [selectedCode, setSelectedCode] = useState(countryCodes[0].code);
@@ -29,17 +29,24 @@ const Booking = () => {
   const safeUserData = userData || { email: null, phone_number: null };
 
   return (
+    // wrap
     <div className="flex flex-col lg:flex-row min-h-screen">
+      <Sidebar />
 
-      <Sidebar />      
-      <div className="flex-1 container mx-auto px-4 py-28 pb-32">
-        <div className="max-w-3xl mx-auto space-y-6">
-          <CheckInInfo />
-          <GuestInfo />
-          <ContactInfo userData={safeUserData} selectedCode={selectedCode} setSelectedCode={setSelectedCode} />
-          <Requests />
-          <AgreementAndPayment roomPrice={roomPrice} />
-        </div>
+      {/* inner */}
+      <div className="max-w-[1000px] space-y-6 flex-1 container mx-auto px-4 py-28 pb-32">
+        <section className="bg-white shadow-lg rounded-lg p-6 w-[892px]">
+          <p className="text-lg font-semibold">체크인 정보</p>
+          <p className="text-neutral-600">예약하시려는 고객님의 정보를 입력해 주세요.</p>
+        </section>
+
+        <GuestInfo />
+
+        <ContactInfo userData={safeUserData} selectedCode={selectedCode} setSelectedCode={setSelectedCode} />
+
+        <Requests />
+
+        <AgreementAndPayment roomPrice={roomPrice} />
       </div>
     </div>
   );
