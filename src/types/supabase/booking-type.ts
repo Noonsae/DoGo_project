36c558df0c4d4547cbc5bbe30c5cf41cf0e49708
Noon_sae <minseock7308@gmail.com>
2025-02-05 14:01:46 +0900

    📝 예약 관련 타입 정리 및 재지정

--- src/types/supabase/booking-type.ts
+++ src/types/supabase/booking-type.ts
@@ -1,3 +1,18 @@
-import { Tables } from "./supabase-type";
+import { Tables } from './supabase-type';
 
 export type BookingDataType = Tables<'bookings'>;
+
+export type PostBookingDataType = Omit<BookingDataType, 'id'>;
+
+export interface fetchBookingDataType extends Tables<'bookings'> {
+  rooms: {
+    room_name: string;
+    hotel_id: string;
+  };
+  hotels: {
+    name: string;
+    address: string;
+    check_in: string;
+    check_out: string;
+  };
+}
