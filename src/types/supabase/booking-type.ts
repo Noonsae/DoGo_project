5269e6cd1c8db44f4c2d8feccf2dc2eb2ac4e0ea
Noon_sae <minseock7308@gmail.com>
2025-02-05 14:07:57 +0900

    📝 PostBookingData 타입 추가 지정

--- src/types/supabase/booking-type.ts
+++ src/types/supabase/booking-type.ts
@@ -2,8 +2,6 @@ import { Tables } from './supabase-type';
 
 export type BookingDataType = Tables<'bookings'>;
 
-export type PostBookingDataType = Omit<BookingDataType, 'id'>;
-
 export interface fetchBookingDataType extends Tables<'bookings'> {
   rooms: {
     room_name: string;
@@ -15,4 +13,18 @@ export interface fetchBookingDataType extends Tables<'bookings'> {
     check_in: string;
     check_out: string;
   };
+}
+
+export interface PostBookingDataType {
+  created_at: string;
+  user_id: string;
+  check_in_date: string;
+  check_out_date: string;
+  hotel_id: string;
+  request: string[] | null;
+  room_id: string;
+  status: string;
+  total_amount: number;
+  user_first_name: string;
+  user_last_name: string;
 }
