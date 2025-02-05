d5610b0cd548ccc700452d801213d3d7a80fff23
Noon_sae <minseock7308@gmail.com>
2025-02-05 14:07:25 +0900

    ✨ 부킹데이터 post api를 호출하는 커스텀훅 생성

---
+++ src/hooks/booking/usePostBookingData.ts
@@ -0,0 +1,17 @@
+import { postBookingData } from '@/utils/postBookingData';
+import { useMutation } from '@tanstack/react-query';
+
+
+const usePostBookingData = () => {
+  return useMutation({
+    mutationFn: postBookingData,
+    onSuccess: (data) => {
+      console.log('Booking successfully created:', data);
+    },
+    onError: (error) => {
+      console.error('Error creating booking:', error.message);
+    }
+  });
+};
+
+export default usePostBookingData;
