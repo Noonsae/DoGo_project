102272a2b474e5b2b88cb1ecd36364963a352f23
wooseok <youkn0wthat@naver.com>
2025-02-06 02:47:09 +0900

    💄 BedTypeList 디자인 시안에 맞게 수정

--- src/app/hotel-list/_components/BedTypeList.tsx
+++ src/app/hotel-list/_components/BedTypeList.tsx
@@ -10,8 +10,8 @@ const bedTypes = ['싱글', '더블', '트윈']; // 침대 타입 배열
 const BedTypeList = ({ selectedBeds, onBedChange }: BedTypeListProps) => {
   return (
     <div>
-      <h3 className="text-lg font-semibold mb-2 mt-10">침대 종류</h3>
-      <ul className="flex flex-col gap-2">
+      <h3 className="text-lg font-semibold mb-[32px] mt-10">침대 종류</h3>
+      <ul className="flex flex-col gap-4">
         {bedTypes.map((bed) => (
           <li key={bed} className="flex items-center gap-2">
             <input
@@ -19,9 +19,9 @@ const BedTypeList = ({ selectedBeds, onBedChange }: BedTypeListProps) => {
               id={`bed-${bed}`}
               checked={selectedBeds.includes(bed)}
               onChange={() => onBedChange(bed)} // `bedType`을 string으로 처리
-              className="w-5 h-5 border-gray-400 rounded-md cursor-pointer"
+              className="w-5 h-5 border-neutral-600 rounded-[9999px] cursor-pointer"
             />
-            <label htmlFor={`bed-${bed}`} className="cursor-pointer">
+            <label htmlFor={`bed-${bed}`} className="text-neutral-600 cursor-pointer">
               {bed}
             </label>
           </li>
