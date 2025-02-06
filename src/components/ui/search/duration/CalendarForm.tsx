0398683ef77640a4e4e2aea9afc633d45f5d76b5
Noon_sae <minseock7308@gmail.com>
2025-02-06 23:53:27 +0900

    💄 캘린더폼 스타일링.. 뭔가 라이브러리 기본 설정을 고치기가 어렵네요 ㅠ

--- src/components/ui/search/duration/CalendarForm.tsx
+++ src/components/ui/search/duration/CalendarForm.tsx
@@ -33,6 +33,13 @@ const CalendarForm = () => {
     return true;
   };
 
+  // 부모 태그의 특정 class를 추가해서 커스텀 스타일링
+  document.querySelectorAll('.child').forEach((child) => {
+    if (child.classList.contains('fc-highlight')) {
+      child.parentElement?.classList.add('parent-highlight');
+    }
+  });
+
   // 첫 번째 달력 클릭 핸들러
   const handleCalendarStartDateClick = (info: { dateStr: string }) => {
     const selectedDate = new Date(info.dateStr);
@@ -112,8 +119,10 @@ const CalendarForm = () => {
           />
         </div>
       </div>
-      <p className="text-sm text-gray-500">
-        선택된 날짜: {checkIn} ~ {checkOut}
+      <p className="text-sm text-gray-700 font-normal">
+        선택된 날짜 : &nbsp;
+        <span className=" text-base text-[#B3916A] font-normal">{checkIn}</span> ~{' '}
+        <span className=" text-base text-[#B3916A] font-normal">{checkOut}</span>
       </p>
     </div>
   );
