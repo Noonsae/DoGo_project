5cc18a5839913aef4b2c967381e31b2e7fcbc1ea
Noon_sae <minseock7308@gmail.com>
2025-02-06 14:03:59 +0900

    📝 로그인이 필요한 서비스입니다 안내문구 수정

--- src/app/booking/_components/TossPaymentsButton.tsx
+++ src/app/booking/_components/TossPaymentsButton.tsx
@@ -10,7 +10,7 @@ const TossPaymentsButton = ({ disabled, bookingData }: { disabled?: boolean; boo
   const clientKey = process.env.NEXT_PUBLIC_TOSS_PAYMENTS_TEST_CLIENT_API_KEY; // 환경 변수 사용
 
   const { data: product } = useHotelNameAndRoomName(bookingData.hotel_id, bookingData.room_id);
-  
+
   const mutation = usePostBookingData();
 
   const { id } = useParams();
@@ -24,11 +24,11 @@ const TossPaymentsButton = ({ disabled, bookingData }: { disabled?: boolean; boo
     const booking_data = await mutation.mutateAsync(bookingData);
 
     if (!booking_data) {
-      return alert('?')
-    };
+      return alert('로그인이 필요한 서비스입니다.');
+    }
 
     const booking_id = booking_data[0].id;
-        
+
     const tossPayments = await loadTossPayments(clientKey);
 
     // TODO_01 : 고유 주문 번호 booking 테이블에서 가져오기
@@ -44,7 +44,6 @@ const TossPaymentsButton = ({ disabled, bookingData }: { disabled?: boolean; boo
         successUrl: `${window.location.origin}/booking/${booking_id}`, // 결제 성공 시 이동할 URL
         failUrl: `${window.location.origin}/booking/fail` // 결제 실패 시 이동할 URL
       });
-
     } catch (error) {
       console.error('결제 요청 중 오류:', error);
     }
