4d0d8713506a6e414cd1d5ad4f4f8b3e7e3bc261
Noon_sae <minseock7308@gmail.com>
2025-02-06 19:14:12 +0900

    🔥  사용하지않는 유틸함수 삭제

--- src/utils/booking/booking.ts
+++ src/utils/booking/booking.ts
@@ -11,15 +11,6 @@ export const saveBookingData = (data: TemporaryBookingData) => {
 };
 
 /**
- * 예약 데이터를 가져오는 함수
- * @returns 저장된 예약 데이터
- */
-export const getBookingData = (): TemporaryBookingData | null => {
-  const bookingData = useBookingStore.getState().temporaryBookingData;
-  return bookingData;
-};
-
-/**
  * 예약 데이터를 초기화하는 함수
  */
 export const clearBookingData = () => {
