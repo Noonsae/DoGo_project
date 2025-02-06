0e8d13a17db83de20ada7b8865723c248d5106d3
wooseok <youkn0wthat@naver.com>
2025-02-06 19:44:13 +0900

    💄 css (1차 수정)

--- src/app/hotel-list/layout.tsx
+++ src/app/hotel-list/layout.tsx
@@ -5,7 +5,7 @@ export default function HotelLayout({ children }: { children: React.ReactNode })
   return (
     <>
       {/* ✅ 모바일에서는 흰색 AuthHeader.tsx, 데스크탑에서는 기존 Header.tsx 유지 */}
-      <div className="md:hidden">
+      <div className="md:hidden fixed z-50 w-full">
         <HotelListHeader />
       </div>
       <div className="hidden md:block">
