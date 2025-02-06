344f0215363ea9485b243f50cde12db5ebb5e02e
Noon_sae <minseock7308@gmail.com>
2025-02-06 19:13:52 +0900

    📝 로컬스토리지에 저장 될 예약일정 관련 데이터의 기본값 설정

--- src/store/useBookingStore.ts
+++ src/store/useBookingStore.ts
@@ -1,18 +1,31 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import { BookingStoreType, TemporaryBookingData } from '@/types/zustand/booking-state-type';
+import { BookingStoreType } from '@/types/zustand/booking-state-type';
 
 // Zustand 스토어 생성
 export const useBookingStore = create<BookingStoreType>()(
   persist(
     (set) => ({
-      temporaryBookingData: null, // 초기 예약 데이터는 null
-      setBookingData: (data: TemporaryBookingData) => set({ temporaryBookingData: data }), // 데이터 저장 함수
-      clearBookingData: () => set({ temporaryBookingData: null }) // 데이터 초기화 함수
+      temporaryBookingData: {
+        checkIn: '', // 기본값 설정
+        checkOut: '',
+        stay: 0,
+        month: 0
+      },
+      setBookingData: (data) => set({ temporaryBookingData: data }),
+      clearBookingData: () =>
+        set({
+          temporaryBookingData: {
+            checkIn: '',
+            checkOut: '',
+            stay: 0,
+            month: 0
+          }
+        })
     }),
     {
-      name: 'booking-storage', // 로컬스토리지 키 이름
-      storage: createJSONStorage(() => localStorage) // 로컬스토리지 사용
+      name: 'booking-storage',
+      storage: createJSONStorage(() => localStorage)
     }
   )
 );
